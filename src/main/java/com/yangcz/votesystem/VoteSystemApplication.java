7b9c6d44ec9c801b49ef331b87a118ec2d097749
web-cz <yangcz.work@gmail.com>
2025-10-03 11:59:48 +0800

    Initialize project

---
+++ src/main/java/com/yangcz/votesystem/VoteSystemApplication.java
@@ -0,0 +1,13 @@
+package com.yangcz.votesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yangcz/votesystem/VoteSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yangcz.votesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
